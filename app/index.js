d8582a320ca17de169521a86873db088727b3b18
Anastasiya Chervona <red02raccoon@gmail.com>
2023-04-06 19:03:10 +0300

    Revert "Merge pull request #5 from Red02Raccoon/feature/students"
    This reverts commit a0c91e9259b7db47483e33081f7ef96ef811f7c9, reversing
    changes made to 43897c486a92e4d268a063633bc0e1ddb9544d6e.

--- app/index.js
+++
@@ -1,3 +0,0 @@
-import { classData } from "./studentsData";
-
-console.log("%c classData ", "background: #222; color: #bada55", { classData });
--- app/studentsData.js
+++
@@ -1,6 +0,0 @@
-export const students = [{ id: 1 }, { id: 2 }];
-
-export const classData = {
-  students,
-  teacher: { id: 1, name: "teacher-name" },
-};
